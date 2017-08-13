ac9d93adabadfba245049724823607c966004a10
Ulrich Schmidt <admin@deruli.de>
2017-08-13 18:03:33 +0200

    erster commit

---
+++ src/content/modules/visitor_country/VisitorCountry.php
@@ -0,0 +1,64 @@
+<?php
+
+class VisitorCountry extends Controller
+{
+
+    private $moduleName = "visitor_country";
+
+    public function beforeHttpHeader()
+    {
+        $controller = ModuleHelper::getMainController("geoPlugin");
+        if (! $controller) {
+            return;
+        }
+        $data = $controller->getGeoLocation();
+        $country = $data["geoplugin_countryName"];
+        if (is_null($this->getCountryValue($val))) {
+            $this->createCountry($country);
+        }
+        $this->countryPlus1($country);
+    }
+
+    protected function createCountry($name)
+    {
+        Database::pQuery("INSERT INTO {prefix}visitor_countries
+                         (name, value) values(?, ?)", array(
+            strval($name),
+            0
+        ), true);
+    }
+
+    protected function getCountryValue($val)
+    {
+        $sql = "select value from {prefix}visitor_countries where name = ?";
+        $args = array(
+            strval($name)
+        );
+        $query = Database::pQuery($sql, $args, true);
+        if (! Database::any($query)) {
+            return null;
+        }
+        $result = Database::fetchObject($query);
+        return $result->value;
+    }
+
+    public function geAllCountries($order = "name")
+    {
+        $sql = "select * from {prefix}visitor_countries order by $order";
+        $query = Database::query($sql, true);
+        $result = array();
+        while ($row = Database::fetchObject($query)) {
+            $result[] = $row;
+        }
+        return $result;
+    }
+
+    protected function countryPlus1($name)
+    {
+        $sql = "Update {prefix}visitor_countries set value = value + 1 where name = ?";
+        $args = array(
+            strval($name)
+        );
+        Database::pQuery($sql, $args, true);
+    }
+}
